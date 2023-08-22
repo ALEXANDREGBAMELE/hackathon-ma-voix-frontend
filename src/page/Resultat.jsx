082718c975307522e90aa0db2c7d4a604913c16e
Alexandre GBAMELE <alexdelor2022@gmail.com>
2023-08-22 14:45:56 +0000

    page resultat

--- src/page/Resultat.jsx
+++ src/page/Resultat.jsx
@@ -1,83 +1,209 @@
 import Collapse from "rc-collapse";
-import React from "react";
+import { React, useEffect, useState } from "react";
 import SondageSideBar from "../components/sideBar/SondageSideBar";
 import ListeCommune from "../components/sideBar/ListeCommune";
+import { Space, Table, Tag } from 'antd';
 
 export function Resultat() {
+  // const [data, setData] = useState([]);
+
+  // useEffect(() => {
+  //   fetch('https://lesinnovateurs.me/api/public/candidats')
+  //     .then(response => response.json())
+  //     .then(apiData => setData(apiData))
+  //     .catch(error => console.error('Erreur lors de la récupération des données:', error));
+  // }, [])
+
+  // const columns = [
+  //   {
+  //     title: 'Photo',
+  //     dataIndex: 'url',
+  //     key: 'url',
+  //     render: (text) => <img src={text} alt="Image" style={{ maxWidth: '60px', borderRadius: "100%" }} />,
+  //   },
+  //   {
+  //     title: 'Nom',
+  //     dataIndex: 'nom',
+  //     key: 'nom',
+  //     render: (text) => <>{text}</>,
+  //   },
+  //   {
+  //     title: 'Prenom',
+  //     dataIndex: 'prenom',
+  //     key: 'prenom',
+  //   },
+  //   {
+  //     title: 'Partie Politique',
+  //     dataIndex: 'partie',
+  //     key: 'partie',
+  //     render: (text) => <img src={text} alt="Image" style={{ maxWidth: '50px' }} />,
+  //   },
+  //   {
+  //     title: 'Commune',
+  //     key: 'commune',
+  //     dataIndex: 'commune',
+  //   },
+  //   {
+  //     title: 'Resultat Sondage(%)',
+  //     key: 'resultat',
+  //     dataIndex: 'resultat',
+  //     render: (text) => <>{text}</>,
+  //   },
+  // ];
+
+
+const columns = [
+  {
+    title: 'Photo',
+    dataIndex: 'url',
+    key: 'url',
+    render: (text) => <img src={text} alt="Image" style={{ maxWidth: '60px', borderRadius: "100%" }} />,
+  },
+  {
+    title: 'Nom',
+    dataIndex: 'nom',
+    key: 'nom',
+    render: (text) => <>{text}</>,
+  },
+  {
+    title: 'Prenom',
+    dataIndex: 'prenom',
+    key: 'prenom',
+  },
+  {
+    title: 'Partie Politique',
+    dataIndex: 'partie',
+    key: 'partie',
+    render: (text) => <img src={text} alt="Image" style={{ maxWidth: '50px' }} />,
+  },
+  {
+    title: 'Commune',
+    key: 'commune',
+    dataIndex: 'commune',
+  },
+  {
+    title: 'Resultat Sondage(%)',
+    key: 'resultat',
+    dataIndex: 'resultat',
+    render: (text) => <>{text}</>
+  },
+];
+
+  const data = [
+    {
+      key: '1',
+      url: 'https://snedai.com/wp-content/uploads/2022/01/PORTRAIT-PDG-1017x1024.jpg',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQGX_PRk7b0bItk4rCyi0MzK1kOINZvo2YpgK_IFBIX&s.png',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+    {
+      key: '1',
+      url: 'url photo',
+      nom: "Adaman",
+      prenom: "Bictogo",
+      partie: 'RHDP',
+      commune: "Yopougon",
+      resultat: "76%"
+    },
+  ];
+
   const bitogoImgUrl = "https://snedai.com/wp-content/uploads/2022/01/PORTRAIT-PDG-1017x1024.jpg"
   return (
-    <div className="container" style={{ display: "flex", gridTemplateColumns: "1fr 1fr", gap: "5px" }}>
-      <div className="side-bar" style={{width:"20rem" }}>
-        <div className="liste-commune" style={{border:"solid 1px e0e0e0", backgroundColor:"white" ,borderRadius:"15px", marginTop:"2rem", padding:"1rem"}}>
-        <ListeCommune></ListeCommune>
+    <div className="container" style={{ display: "flex", gridTemplateColumns: "1fr 1fr", gap: "5px", overflow: "hidden", height: "calc(100vh - 11 rem)" }}>
+      <div className="side-bar" style={{ width: "26rem" }}>
+        <div className="liste-commune" style={{ border: "solid 1px e0e0e0", backgroundColor: "white", borderRadius: "15px", marginTop: "1rem", padding: "2rem" }}>
+          <h3 style={{ textAlign: "center" }}>Liste des communes</h3><br />
+          <ListeCommune></ListeCommune>
         </div>
       </div>
 
-      <div className="resultat-container" style={{ overflowY:"auto", msOverflowStyle:"none",width:"100%"}}>
-        <div className="top-card" style={{ display: "flex", gridTemplateColumns:"1fr 1fr" , gap:"10px", marginLeft:"3.7rem", marginTop:"2rem"}}>
+      <div className="resultat-container" style={{ width: "100%", }}>
+        <div className="top-card" style={{ display: "flex", gridTemplateColumns: "1fr 1fr", gap: "10px", marginLeft: "", marginTop: "1rem", backgroundColor: "white", borderRadius: "15px" }}>
           <div className="left" style={{ width: "400px", }}>
-            <img style={{borderBottomRightRadius:"15px", borderBottomLeftRadius:"15px"}} src={bitogoImgUrl} alt="" />
+            <img style={{ borderBottomRightRadius: "15px", borderRadius: "15px" }} src={bitogoImgUrl} alt="" />
           </div>
           <div className="right">
-            <div className="title" style={{ fontSize: "24px", padding:"5px", textAlign:"center" }}>Au Somet : </div>
-            <div className="nome-candidat-winer" style={{ fontSize: "24px", padding:"5px", textAlign:"center" }}><h3>Adaman Bictogo</h3></div>
-            <div className="total-vote" style={{ fontSize: "24px", padding:"5px", textAlign:"center" }}>Total vote : </div>
-            <div className="score" style={{ fontSize: "24px", padding:"5px", textAlign:"center" }}>Score : 77%</div>
+            <div className="title" style={{ fontSize: "24px", padding: "5px", textAlign: "center" }}>Au Somet : </div>
+            <div className="nome-candidat-winer" style={{ fontSize: "24px", padding: "5px", textAlign: "center" }}><h3>Adaman Bictogo</h3></div>
+            <div className="total-vote" style={{ fontSize: "24px", padding: "5px", textAlign: "center" }}>Total vote : </div>
+            <div className="score" style={{ fontSize: "24px", padding: "5px", textAlign: "center" }}>Score : 77%</div>
           </div>
         </div>
 
 
-        <div className="bottom-card" style={{ display: "flex", flexDirection: "row", borderRadius: "15px", marginTop:"15px", marginBottom:"6rem" }}>
-          <div className="item-1" style={{ display: "flex", gridTemplateColumns: "1fr 1fr", gap: "70rem", marginLeft: "5%", padding: "10px", marginBottom: "-15px" , borderRadius:"15px" , border:"solid 1px e0e0e0", backgroundColor:"white"}}>
-            <table className="users-table" style={{ borderCollapse: "collapse", marginTop: "40px", borderRadius:"15px" , backgroundColor:"#e0e0e0"}}>
-              <thead>
-                <tr style={{backgroundColor:"#fff"}}>
-                  <th style={{ padding: "10px" }} >Photo</th>
-                  <th style={{ padding: "10px" }} >Nom</th>
-                  <th style={{ padding: "10px" }} >Prénom</th>
-                  <th style={{ padding: "10px" }}>commune</th>
-                  <th style={{ padding: "10px" }}>Resultat</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr style={{ backgroundColor: "#e0e0e0" }}>
-                  <td style={{ padding: "10px" }}>
-                    <img src={bitogoImgUrl} alt="Photo utilisateur" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
-                  </td>
-                  <td style={{ padding: "10px" }}>Adaman</td>
-                  <td style={{ padding: "10px" }}>Bitogo presiden de l'asseble Nationnale</td>
-                  <td style={{ padding: "10px" }}>Commune de Yopougon</td>
-                  <td style={{ padding: "10px" }}>77%</td>
-                </tr>
-                <tr style={{ backgroundColor:"#fff" }}>
-                  <td style={{ padding: "10px" }}>
-                    <img src={bitogoImgUrl} alt="Photo utilisateur" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
-                  </td>
-                  <td style={{ padding: "10px" }}>Adaman</td>
-                  <td style={{ padding: "10px" }}>Bitogo</td>
-                  <td style={{ padding: "10px" }}>Yopougon</td>
-                  <td style={{ padding: "10px" }}>77%</td>
-                </tr>
-                <tr style={{ backgroundColor: "#e0e0e0" }}>
-                  <td style={{ padding: "10px" }}>
-                    <img src={bitogoImgUrl} alt="Photo utilisateur" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
-                  </td>
-                  <td style={{ padding: "10px" }}>Adaman</td>
-                  <td style={{ padding: "10px" }}>Bitogo</td>
-                  <td style={{ padding: "10px" }}>Yopougon</td>
-                  <td style={{ padding: "10px" }}>77%</td>
-                </tr>
-                <tr  style={{backgroundColor:"#fff"}}>
-                  <td style={{ padding: "10px" }}>
-                    <img src={bitogoImgUrl} alt="Photo utilisateur" style={{ width: "50px", height: "50px", borderRadius: "50%" }} />
-                  </td>
-                  <td style={{ padding: "10px" }}>Adaman</td>
-                  <td style={{ padding: "10px" }}>Bitogo</td>
-                  <td style={{ padding: "10px" }}>Yopougon</td>
-                  <td style={{ padding: "10px" }}>77%</td>
-                </tr>
-              </tbody>
-            </table>
+        <div className="bottom-card" style={{ display: "flex", flexDirection: "row", borderRadius: "15px", marginTop: "1rem", marginBottom: "6rem", width: "100%" }}>
+          <div className="table">
+            <Table columns={columns} dataSource={data}
+              scroll={{
+                y: 240,
+              }}
+            />
           </div>
         </div>
       </div>
