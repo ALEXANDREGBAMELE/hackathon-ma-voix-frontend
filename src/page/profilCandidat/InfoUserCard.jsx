88f20a400f38a7890e95950d708d3f4c88c035d9
Alexandre GBAMELE <alexdelor2022@gmail.com>
2023-08-25 07:09:47 +0000

    mini dashboard profil candidat

--- src/page/profilCandidat/InfoUserCard.jsx
+++ src/page/profilCandidat/InfoUserCard.jsx
@@ -12,24 +12,24 @@ const user = {
 
 const UserInfoCard = () => {
   return (
-    <div className="user-info-card" style={{backgroundColor:"",display:"flex",padding:".5rem"}}>
-      <div className="user-photo" style={{width:"50%", justifyContent:"center",textAlign:"end", padding:""}}>
-        <img size={150} src={user.photoUrl}  style={{marginRight:""}}/>
+    <div className="user-info-card" style={{ backgroundColor: "", display: "flex", padding: ".5rem" }}>
+      <div className="user-photo" style={{ width: "50%", justifyContent: "center", textAlign: "end",backgroundColor:"#e0e0e0" }}>
+        <img size={150} src={user.photoUrl} style={{ marginRight: "" }} />
       </div>
-      <div className="user-details" style={{width:"50%", backgroundColor:"white", textAlign:"center"}}>
-        <h2 className="user-name" style={{fontSize:"25px", marginTop:"2rem"}}>
+      <div className="user-details" style={{ width: "50%", textAlign: "center", backgroundColor: "#027314", color:"white" }}>
+        <h2 className="user-name" style={{ fontSize: "28px", marginTop: "2rem" }}>
           {user.firstName} {user.lastName}
         </h2>
         <p className="user-email">{user.email}</p>
-        <p className="user-description">{user.description}</p>
-        <div className="vision" style={{border:"1px green solid", height:"300px", width:"450px", marginLeft:"15rem", marginTop:"2rem"}}>
-          <h3 style={{borderBottom:"solid 5px green", padding:"1rem"}}>Vision</h3>
-          <hr/>
-        <p style={{ padding:"1rem", fontSize:"20"}}>
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora libero odio necessitatibus culpa cupiditate! Aliquam similique ratione natus. Quia veritatis, fuga recusandae ex laborum molestias doloremque quae? Impedit, vel accusamus. <br />
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora libero odio necessitatibus culpa cupiditate! Aliquam similique ratione natus. Quia veritatis, fuga recusandae ex laborum molestias doloremque quae? Impedit, vel accusamus.
-        </p>
-      </div>
+        <p className="user-description" style={{ fontSize: "25px", marginTop: "1rem" }}>{user.description}</p>
+        <div className="vision" style={{ border: "1px green solid", height: "300px", width: "550px", marginLeft: "15rem", marginTop: "2rem" }}>
+          <h3 style={{ borderBottom: "solid 5px green", padding: "1rem",backgroundColor:"#ff7200", fontSize:"28px" }}>Vision</h3>
+          <hr />
+          <p style={{ padding: "1rem", fontSize: "18px", backgroundColor:"#" }}>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora libero odio necessitatibus culpa cupiditate! Aliquam similique ratione natus. Quia veritatis, fuga recusandae ex laborum molestias doloremque quae? Impedit, vel accusamus. <br />
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tempora libero odio necessitatibus culpa cupiditate! Aliquam similique ratione natus. Quia veritatis, fuga recusandae ex laborum molestias doloremque quae? Impedit, vel accusamus.
+          </p>
+        </div>
       </div>
     </div>
   );
