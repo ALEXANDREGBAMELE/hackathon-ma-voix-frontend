68b404c2577f40035bb542bd904842ba4dac934a
Tonde Souleymane <tondesoulco@gmail.com>
2023-08-29 11:37:04 +0000

    ultimatum

--- src/page/profilCandidat/CandidatAllPosts.jsx
+++ src/page/profilCandidat/CandidatAllPosts.jsx
@@ -9,187 +9,203 @@ import {
 } from "@ant-design/icons";
 import CommentModal from "../../components/candidatCard/CommentModal.jsx";
 import ImageModal from "../../components/candidatCard/ImageModal.jsx";
-import { getMyPosts } from "../../app/services/candidat.js";
-import { getAllPostLikes, getPostComments } from "../../app/services/public.js";
+import { getMyPosts,  } from "../../app/services/candidat.js";
+import { getAllPostLikes, getPostComments , removeLike, addLike, addComment} from "../../app/services/public.js";
 
-const CandidatAllPosts = () => {
-  const [posts, setPosts] = useState([]);
+const CandidatAllPosts = ({post}) => {
+  const url = `https://lesinnovateurs.me/${post.url_media}`;
+  const [totalLikes, setTotalLikes] = useState(0);
+  const [totalComments, setTotalComments] = useState(0);
+  const [isLiked, setIsLiked] = useState(false);
   const [showFullText, setShowFullText] = useState(false);
   const [showImageModal, setShowImageModal] = useState(false);
   const [showCommentModal, setShowCommentModal] = useState(false);
-  const [selectedPost, setSelectedPost] = useState(null);
+
+  //s'il n'y aucun post, display le empty icon
   const [emptyPost, setEmptyPost] = useState(false);
-  const [messageApi, contextHolder] = message.useMessage();
+
+  const containsHtmlTags = /<[^>]*>/.test(post.description);
 
   const dispatch = useDispatch();
   const User = JSON.parse(localStorage.getItem("logUser"));
-  const tokenUser = JSON.parse(localStorage.getItem("token"));
+  let tokenUser = JSON.parse(localStorage.getItem("token"));
 
   const photo_candidat =
-    User.photo_url && !User.photo_url.includes("cloudinary")
-      ? `https://lesinnovateurs.me/${User.photo_url}`
-      : User.photo_url;
+    post.candidat.user.photo_url &&
+    !post.candidat.user.photo_url.includes("cloudinary")
+      ? `https://lesinnovateurs.me/${post.candidat.user.photo_url}`
+      : post.candidat.user.photo_url;
+
+  const url_media =
+    post.url_media && !post.url_media.includes("cloudinary")
+      ? `https://lesinnovateurs.me/${post.url_media}`
+      : post.url_media;
 
   useEffect(() => {
-    const fetchPosts = async () => {
+    const fetchData = async () => {
+      // Fetch likes
+      const allPostLikes = (await getAllPostLikes(post.id)).data.data;
+      setTotalLikes(allPostLikes.length);
+      const userLiked = allPostLikes.some((like) => like.id_user == User?.id);
+      setIsLiked(userLiked);
+
       try {
-        const response = await getMyPosts();
-        if (response.message || !response.posts) {
-          setPosts([]);
-          return;
+        const response = await getPostComments(post.id);
+        if (response.data.message === "Pas de commentaires") {
+          setTotalComments(0); // Aucun commentaire trouvé
+        } else {
+          setTotalComments(response.data.data.length);
         }
-        console.log("response posts", response.posts);
-        setPosts(response.posts);
       } catch (error) {
-        console.error("Erreur lors de la récupération des posts :", error);
+        console.error(
+          "Erreur lors de la récupération des commentaires :",
+          error
+        );
       }
     };
 
-    fetchPosts();
-  }, []);
+    fetchData();
+  }, [totalComments, totalLikes]);
 
-  const handleLike = async (post) => {
-    const response = await getAllPostLikes(post.id);
-    const response2 = await getPostComments(post.id);
+  const [messageApi, contextHolder] = message.useMessage();
 
-    if (response.message || !response.likes) {
+  const handleLike = async () => {
+    if (!User) {
+      await messageApi.open({
+        type: "error",
+        content: "vous devez vous connecter pour effectuer cette action",
+      });
       return;
     }
 
-    if (response2.message || !response2.comments) {
-      return;
-    }
-
-    const updatedPosts = posts.map((p) => {
-      if (p.id === post.id) {
-        return {
-          ...p,
-          isLiked: !p.isLiked,
-          totalLikes: response.likes.length,
-          totalComments: response2.comments.length,
-        };
+    try {
+      if (isLiked) {
+        const unlike = await removeLike(User.id, post.id, tokenUser);
+        if (unlike.success) {
+          setIsLiked(false);
+          setTotalLikes(totalLikes - 1);
+          dispatch(decrementLikes(post.id));
+        } else {
+          await messageApi.open({
+            type: "warning",
+            content: "Une erreur s'est produite lors de la gestion du like.",
+          });
+        }
+      } else {
+        const like = await addLike(User.id, post.id, tokenUser);
+        console.log(like);
+        if (like.success) {
+          setIsLiked(true);
+          setTotalLikes(totalLikes + 1);
+          dispatch(incrementLikes(post.id));
+        } else {
+          await messageApi.open({
+            type: "warning",
+            content: "Une erreur s'est produite lors de la gestion du like.",
+          });
+        }
       }
-      return p;
-    });
-
-    setPosts(updatedPosts);
+    } catch (error) {
+      console.error("Erreur lors de la gestion du like :", error);
+      await messageApi.open({
+        type: "warning",
+        content: "Une erreur s'est produite lors de la gestion du like.",
+      });
+    }
   };
 
-  const handleComment = (post) => {
-    setShowCommentModal(true);
-    setSelectedPost(post.id);
-  };
-
-  const handleImageClick = (url) => {
-    setShowImageModal(url);
+  const handleAvatarClick = async () => {
+    window.location.href = `/candidat/${post.candidat.id}`;
   };
 
   return (
-    <>
-      {posts.length === 0 ? (
-        <Card className="facebook-post-card no-posts">No posts available.</Card>
-      ) : (
-        posts.map((post) => (
-          <Card
-            key={post.id}
-            className={`facebook-post-card animated fadeIn ${
-              showFullText ? "expanded-card" : ""
-            }`}
-          >
-            <div className="post-content">
-              <div className="post-header">
-                {contextHolder}
-                <div className="post-avatar">
-                  <a>
-                    <Avatar src={photo_candidat} />
-                    <span className="candidat-name badge success">
-                      {post.candidat.user.nom + " " + post.candidat.user.prenom}
-                    </span>
-                  </a>
-                </div>
-                <div className="post-title">{post.titre}</div>
-              </div>
-              <div
-                className={`post-text ${showFullText ? "expanded-text" : ""}`}
-              >
-                {post.description.includes("< >") ? (
-                  <div
-                    dangerouslySetInnerHTML={{ __html: post.description }}
-                    className="html-content"
-                  />
-                ) : (
-                  <>
-                    {showFullText || post.description.length <= 150
-                      ? post.description
-                      : post.description.slice(0, 150) + "..."}
-                    {post.description.length > 150 && (
-                      <span
-                        className="see-more badge"
-                        onClick={() => setShowFullText(!showFullText)}
-                      >
-                        {showFullText ? "Voir moins" : "Voir plus"}
-                      </span>
-                    )}
-                  </>
-                )}
-              </div>
-            </div>
+    <Card
+      className={`facebook-post-card animated fadeIn ${
+        showFullText ? "expanded-card" : ""
+      }`}
+    >
+      <div className="post-content">
+        <div className="post-header">
+          {contextHolder}
+          <div className="post-avatar">
+            <a onClick={handleAvatarClick}>
+              <Avatar src={photo_candidat} />
+              <span className="candidat-name badge success">
+                {post.candidat.user.nom + " " + post.candidat.user.prenom}
+              </span>
+            </a>
+          </div>
+          <div className="post-title">{post.titre}</div>
+        </div>
+        <div className={`post-text ${showFullText ? "expanded-text" : ""}`}>
+          {containsHtmlTags ? (
             <div
-              className="post-image-container animated fadeInLeft"
-              onClick={() => setShowImageModal(true)}
-            >
-              <img
-                src={
-                  post.url_media && !post.url_media.includes("cloudinary")
-                    ? `https://lesinnovateurs.me/${post.url_media}`
-                    : post.url_media
-                }
-                alt="Image du post"
-                className="post-image"
-              />
-            </div>
-            <div className="action-buttons">
-              <Button
-                onClick={() => handleLike(post)}
-                className={`like-button ${post.isLiked ? "liked" : ""}`}
-                icon={post.isLiked ? <LikeFilled /> : <LikeOutlined />}
-                key="like"
-              >
-                {post.totalLikes}
-              </Button>
-              <Button
-                icon={<MessageFilled />}
-                onClick={() => handleComment(post)}
-                key="comment"
-                className="comment-button"
-              >
-                {post.totalComments}
-              </Button>
-              <Button
-                icon={<SendOutlined />}
-                key="share"
-                className="share-button"
-              />
-            </div>
-            <CommentModal
-              postId={post.id}
-              visible={showCommentModal && selectedPost === post.id}
-              onClose={() => setShowCommentModal(false)}
+              dangerouslySetInnerHTML={{ __html: post.description }}
+              className="html-content"
             />
-            <ImageModal
-              open={showImageModal}
-              imageUrl={
-                post.url_media && !post.url_media.includes("cloudinary")
-                  ? `https://lesinnovateurs.me/${post.url_media}`
-                  : post.url_media
-              }
-              onClose={() => handleImageClick(false)}
-            />
-          </Card>
-        ))
-      )}
-    </>
+          ) : (
+            <>
+              {showFullText || post.description.length <= 150
+                ? post.description
+                : post.description.slice(0, 150) + "..."}
+              {post.description.length > 150 && (
+                <span
+                  className="see-more badge"
+                  onClick={() => setShowFullText(!showFullText)}
+                >
+                  {showFullText ? "Voir moins" : "Voir plus"}
+                </span>
+              )}
+            </>
+          )}
+        </div>
+      </div>
+      <div
+        className="post-image-container animated fadeInLeft"
+        onClick={() => setShowImageModal(true)}
+      >
+        <img
+          src={url_media}
+          style={{
+            maxHeight: "25rem",
+            objectFit: "cover",
+          }}
+          alt="Image du post"
+          className="post-image"
+        />
+      </div>
+      <div className="action-buttons">
+        <Button
+          onClick={handleLike}
+          className={`like-button ${isLiked ? "liked" : ""}`}
+          icon={isLiked ? <LikeFilled /> : <LikeOutlined />}
+          key="like"
+        >
+          {totalLikes}
+        </Button>
+        <Button
+          icon={<MessageFilled />}
+          onClick={() => setShowCommentModal(true)}
+          key="comment"
+          className="comment-button"
+        >
+          {totalComments}
+        </Button>
+
+        <Button icon={<SendOutlined />} key="share" className="share-button" />
+      </div>
+
+      <CommentModal
+        postId={post.id}
+        visible={showCommentModal}
+        onClose={() => setShowCommentModal(false)}
+      />
+      <ImageModal
+        open={showImageModal}
+        imageUrl={url}
+        onClose={() => setShowImageModal(false)}
+      />
+    </Card>
   );
 };
-
 export default CandidatAllPosts;
